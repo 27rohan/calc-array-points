8a8dc4b3c5a3ad19cebdd81e9f1d41d73bd9a26c
Rohan Thakkar <rohanthakkar@d-10-157-195-138.dhcp4.washington.edu>
2016-10-11 08:54:02 -0700

    Initial Commit

---
+++ calc-array-points/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calc-array-points
+//
+//  Created by Rohan Thakkar on 10/11/16.
+//  Copyright © 2016 Rohan Thakkar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
